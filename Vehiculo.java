9cf0976278ab214815a6fc5e563a59673d20ae83
Cristina-Soto-Lab <cristina.soto@uchile.cl>
2025-08-28 17:59:06 -0400

    Add files via upload

---
+++ Vehiculo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Vehiculo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Pedir datos
+        System.out.print("Ingrese la marca: ");
+        String marca = sc.nextLine();
+
+        System.out.print("Ingrese el modelo: ");
+        String modelo = sc.nextLine();
+
+        System.out.print("Ingrese la cilindrada: ");
+        String cilindrada = sc.nextLine();
+
+        System.out.print("Ingrese el tipo de combustible: ");
+        String combustible = sc.nextLine();
+
+        System.out.print("Ingrese la capacidad en pasajeros: ");
+        int pasajeros = sc.nextInt();
+
+        // Mostrar resultados
+        System.out.println("La marca que ha ingresado es: " + marca);
+        System.out.println("El modelo que ha ingresado es: " + modelo);
+        System.out.println("La cilindrada que ha ingresado es: " + cilindrada);
+        System.out.println("El tipo de combustible es: " + combustible);
+        System.out.println("Tiene una capacidad de " + pasajeros + " pasajeros.");
+    }
+}
